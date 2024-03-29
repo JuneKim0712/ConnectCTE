aad31a36154f252a5e71362690c0cdb53fd28fda
June <Junwoo.kim0712@gmail.com>
2024-03-28 18:00:58 -0700

    added google sign in and background img

--- public/script.js
+++ public/script.js
@@ -1,20 +1,16 @@
 const auth = firebase.auth()
 
 // Elements
-const elements = {
-  whenSignedOut: document.getElementById('whenSignedOut'),
-  signInBtn: document.getElementById('signInBtn')
-}
+const signInBtn = document.querySelector('.gsi-material-button');
 
 // Firebase Auth & Providers
 const provider = new firebase.auth.GoogleAuthProvider()
 
 // Event listeners
-elements.signInBtn.onclick = () => auth.signInWithRedirect(provider)
+signInBtn.onclick = () => auth.signInWithRedirect(provider)
 
 auth.onAuthStateChanged(user => {
   if (user) {
     window.location.href = '/home.html'
   }
-  elements.whenSignedOut.hidden = false
 })
